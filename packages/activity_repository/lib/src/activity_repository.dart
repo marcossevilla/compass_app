be6f12a7e2eba00b54a932e9c8a3e91b130a85ee
Marcos Sevilla <marcossevilla14@gmail.com>
2025-01-28 16:34:40 +0100

    feat: add activity repository

--- packages/activity_repository/lib/src/activity_repository.dart
+++ packages/activity_repository/lib/src/activity_repository.dart
@@ -1,7 +1,26 @@
+import 'package:api_client/api_client.dart';
+
 /// {@template activity_repository}
 /// Repository that manages the activity domain.
 /// {@endtemplate}
 class ActivityRepository {
   /// {@macro activity_repository}
-  const ActivityRepository();
+  ActivityRepository({required ApiClient apiClient}) : _apiClient = apiClient;
+
+  final ApiClient _apiClient;
+
+  final Map<String, List<Activity>> _cachedData = {};
+
+  /// Get activities by destination reference.
+  Future<List<Activity>> getByDestination(String ref) async {
+    // Return cached data if available.
+    if (_cachedData.containsKey(ref)) return _cachedData[ref]!;
+
+    // No cached data, request activities.
+    final result = await _apiClient.getActivityByDestination(ref);
+
+    _cachedData[ref] = result;
+
+    return result;
+  }
 }
--- packages/activity_repository/test/src/activity_repository_test.dart
+++ packages/activity_repository/test/src/activity_repository_test.dart
@@ -1,11 +1,21 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:activity_repository/activity_repository.dart';
+import 'package:api_client/api_client.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
+
+class _MockApiClient extends Mock implements ApiClient {}
 
 void main() {
   group('ActivityRepository', () {
+    late ApiClient apiClient;
+
+    setUp(() {
+      apiClient = _MockApiClient();
+    });
+
     test('can be instantiated', () {
-      expect(ActivityRepository(), isNotNull);
+      expect(ActivityRepository(apiClient: apiClient), isNotNull);
     });
   });
 }
